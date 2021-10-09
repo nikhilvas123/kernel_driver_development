daeb0988edbf8e1876c516e57d3a2f371a081047
Nikhil Vas <nikhilvas123@gmail.com>
2021-10-09 16:05:16 +0530

    Fixed errors and added makefile

--- pseudo_char/pchar_driver.c
+++ pseudo_char/pchar_driver.c
@@ -1,4 +1,8 @@
 #include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
 #define MINOR_BASE 0
 #define DRIVER_NAME "pseudo"
 dev_t pdevid;
@@ -24,3 +28,10 @@ static void __exit pseudo_exit(void)
     unregister_chrdev_region(pdevid, ndevices);
     printk("Psuedo: Driver unregistered\n");
 }
+
+module_init(pseudo_init);
+module_exit(pseudo_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Nikhil");
+MODULE_DESCRIPTION("Pseudo character driver");
